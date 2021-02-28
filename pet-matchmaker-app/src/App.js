5ea60673c6dbb0a5946c09bc0a642c0758c9ce5a
Gretchen Marie <gmarie@ggc.edu>
2021-02-27 22:29:31 -0500

    Few more changes to the UI

--- pet-matchmaker-app/src/App.js
+++ pet-matchmaker-app/src/App.js
@@ -5,18 +5,18 @@ function App() {
   return (
     <div className="App">
       <div className="w3-theme-orange container">
-      <div className="w3-theme-d1 container" id="summary">
-        <p>Summary:<br/>
+        <div className="w3-theme-d1 container" id="summary">
+          <p>Summary:<br />
         Inspired by early 2010s personality quizzes,
-        Pet Matchmaker is a web app that matches you to the best 
+        Pet Matchmaker is a web app that matches you to the best
         possible pet. The quiz is simple and takes many factors into account such as:
         home environment, attentional availability, emotional needs, budget, allergies, and more!
         Click the start button below to find <i>your</i> perfect pet!
       </p>
+        </div>
+          <button className="button">START</button>
       </div>
     </div>
-
-  </div>
   );
 }
 
